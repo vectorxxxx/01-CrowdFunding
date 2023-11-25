cfba0ed6479a670921d9465085112ea318dd0de6
vectorx <uxiahnan@outlook.com>
2023-11-25 14:19:50 +0800

    feat: session共享测试

---
+++ spring-session-b/src/main/java/xyz/funnyboy/springsession/springsessionb/SpringSessionBApplication.java
@@ -0,0 +1,14 @@
+package xyz.funnyboy.springsession.springsessionb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionBApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSessionBApplication.class, args);
+    }
+
+}
